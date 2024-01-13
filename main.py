00a5f274fa2017e330d9e5ae84ea887b7f1b3e30
xilinlan <xilinlan@cug.edu.cn>
2024-01-13 17:09:51 +0800

    增加main.py中的num_workers，以及创建samples文件夹

--- main.py
+++ main.py
@@ -18,20 +18,20 @@ def main(config):
     if config.train:
         # Create directories if not exist
         make_folder(config.model_save_path, config.arch)
-        # make_folder(config.sample_path, config.arch)
+        make_folder(config.sample_path, config.arch)
 
         transform = Compose([RandomHorizontallyFlip(p=.5), RandomSized(
             size=config.imsize), AdjustBrightness(bf=0.1), AdjustContrast(cf=0.1), AdjustHue(hue=0.1),
                              AdjustSaturation(saturation=0.1)])
         data_loader = CustomDataLoader(config.img_path, config.label_path, config.imsize,
-                                       config.batch_size, transform=transform, mode=config.train)
+                                       config.batch_size, config.num_workers, transform=transform, mode=config.train)
         val_loader = CustomDataLoader(config.val_img_path, config.val_label_path, config.imsize,
-                                      config.batch_size, transform=None, mode=bool(1 - config.train))
+                                      config.batch_size, config.num_workers, transform=None, mode=bool(1 - config.train))
         trainer = Trainer(data_loader.loader(), config, val_loader.loader())
         trainer.train()
     else:
         data_loader = CustomDataLoader(config.test_image_path, config.test_label_path, config.imsize,
-                                       config.batch_size, mode=config.train)
+                                       config.batch_size, config.num_workers, mode=config.train)
         tester = Tester(data_loader.loader(), config)
         tester.test()
 
