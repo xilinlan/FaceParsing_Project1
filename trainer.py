45ff518fd206b2866edbb4a8304d25b8a14b9967
xilinlan <xilinlan@cug.edu.cn>
2024-01-15 23:41:36 +0800

    modify:修改记录网络结构到tensorboard的操作

--- trainer.py
+++ trainer.py
@@ -62,9 +62,6 @@ class Trainer(object):
             config.model_save_path, self.arch)
 
         self.build_model()
-        # Add the model graph to TensorBoard
-        dummy_input = torch.zeros([1, 3, self.imsize, self.imsize]).cuda()
-        self.writer.add_graph(self.G, dummy_input)
 
         # Start with trained model
         if self.pretrained_model:
@@ -107,6 +104,9 @@ class Trainer(object):
         progress_bar = tqdm(range(start, self.epochs), desc='Epoch-Check', mininterval=10, maxinterval=60, ncols=100)
         for epoch in progress_bar:
             self.G.train()
+            if epoch == 0:
+                # 记录网络结构到tensorboard
+                self.writer.add_graph(self.G, torch.rand(1, 3, self.imsize, self.imsize).cuda())
             for i_iter, batch in enumerate(self.data_loader):
                 i_iter += len(self.data_loader) * epoch
                 # lr = adjust_learning_rate(self.g_lr,
